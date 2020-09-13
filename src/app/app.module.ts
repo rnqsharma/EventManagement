4050bd4236fbb863c47207c6427447a959c2d836
Raghav Sahni <32719066+raghavsahni@users.noreply.github.com>
2020-09-14 01:23:43 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HowItWorksComponent,
+    PortfolioComponent,
+    HeaderComponent,
+    AboutUsComponent,
+    ContactUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
