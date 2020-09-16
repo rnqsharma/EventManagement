c800de6633ddbd9261908f954a51dc344e70b66c
Raghav Sahni <32719066+raghavsahni@users.noreply.github.com>
2020-09-16 16:20:38 +0530

    about

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-
+  type = [ 'Personal', 'Organizational', 'Educational', 'Extra-Curricular', 'Tours'];
+  name;
+  phone;
+  email;
+  more
+  confirmemail;
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+  onSubmit(form:NgForm){
+    console.log(form.value)
   }
 
 }
