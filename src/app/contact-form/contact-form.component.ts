02c5d3994b4739c1df9c494cf08167415aa615f6
Raunaq <rnqsharma3@gmail.com>
2020-09-22 00:23:40 +0530

    Added Regex

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, ElementRef, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,26 +7,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+
   type = [ 'Personal', 'Organizational', 'Educational', 'Extra-Curricular', 'Tours'];
   contactForm : FormGroup;
-  phonePattern = '^((\+){0,1}91(\s){0,1}(\-){0,1}(\s){0,1}){0,1}98(\s){0,1}(\-){0,1}(\s){0,1}[1-9]{1}[0-9]{7}$';
+  phonePattern = `^((\\+91-?)|0)?[0-9]{10}$`;
   emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
   constructor() { }
 
   ngOnInit() {
+
     this.contactForm = new FormGroup ({
       'name': new FormControl(null , Validators.required),
       'email': new FormControl('', [Validators.required, Validators.email]),
-      //'phone': new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
-      'phone': new FormControl(null , Validators.required ),
+      'phone': new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
+      // 'phone': new FormControl(null , Validators.required),
       'eventType': new FormControl(null),
       'eventDetails': new FormControl(null),
-
       
     })
-
   }
+
   onSubmit(){
     
     console.log(this.contactForm.value);
