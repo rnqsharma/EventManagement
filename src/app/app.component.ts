76fed4f2d66fbd019b8279f5990569c73c4e96e5
Raghav Sahni <32719066+raghavsahni@users.noreply.github.com>
2020-09-25 04:24:27 +0530

    Cards and Scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'EventManagment';
+  title = 'Deventure Select';
+
+//   onActivate(event) {
+//     let scrollToTop = window.setInterval(() => {
+//         let pos = window.pageYOffset;
+//         if (pos > 0) {
+//             window.scrollTo(0, pos - 20); // how far to scroll on each step
+//         } else {
+//             window.clearInterval(scrollToTop);
+//         }
+//     }, 8);
+// }
+
+onActivate(event) {
+  window.scroll(0,0);
+  
+}
 }
